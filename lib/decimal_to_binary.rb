e59a15454d48e8c967ee64053ef5b73d68ea3d09
Ray Su <ray.su@shoplineapp.com>
2020-03-12 00:01:00 +0800

    fix(DecimalToBianry): typo

--- lib/decimal_to_binary.rb
+++ lib/decimal_to_binary.rb
@@ -1,5 +1,5 @@
 module DecimalToBianry
-  # Givin a positive number n, please implement a function that returns a binary string of n.
+  # Given a positive number n, please implement a function that returns a binary string of n.
   # e.g. n = 100, output: "1100100"
   # Do not use Integer#to_s(base)
   def self.call num
